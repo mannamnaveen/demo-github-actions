a0499be82e3115f0d4c9c58ec1103e6f4ea9abe6
mannamnaveen <mina@naveenmannam.com>
2023-05-11 17:32:18 -0400

    Updated the terraform.yaml file

--- main.tf
+++ main.tf
@@ -10,14 +10,6 @@ terraform {
     }
   }
   required_version = "~> 1.0"
-
-  backend "remote" {
-    organization = "REPLACE_ME"
-
-    workspaces {
-      name = "REPLACE_ME"
-    }
-  }
 }
 
 
@@ -30,7 +22,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-09e67e426f25ce0d7"
+  ami                    = "ami-0889a44b331db0194"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
